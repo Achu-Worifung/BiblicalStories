b328df4994468bb795026a33e23512c48ac53b61
worifung achu <achuclinton47@gmail.com>
2025-05-15 20:17:54 -0700

    feat:create new component --create the story of the day component using 2d card --whitelisted azure image container

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-import { FloatingNav } from "@/components/ui/floating-nav";
+import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
 import { FocusCards } from "@/components/ui/focus-card";
+import {SOTD} from "@/components/ui/story-of-the-day";
 // import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
 
 export default function Home() {
@@ -33,6 +34,71 @@ export default function Home() {
     <div className="w-screen flex flex-col items-center justify-center text-white">
 
      <FocusCards cards={cards}/>
+
+      <CardContainer className="inter-var">
+      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-lvh  h-auto rounded-xl p-6 border flex gap-4">
+        {/* <CardItem
+          translateZ="50"
+          className="text-xl font-bold text-neutral-600 dark:text-white"
+        >
+          Make things float in air
+        </CardItem>
+        <CardItem
+          as="p"
+          translateZ="60"
+          className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+        >
+          Hover over this card to unleash the power of CSS perspective
+        </CardItem> */}
+        <CardItem translateZ="100" className="w-2/3 mt-4">
+          <img
+            src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            height="1000"
+            width="1000"
+            className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+            alt="thumbnail"
+          />
+        </CardItem>
+        <div className="flex justify-between items-left  flex-col">
+          <CardItem
+            translateZ={20}
+            as="a"
+            href="https://twitter.com/mannupaaji"
+            target="__blank"
+            className="text-3xl font-bold text-neutral-600 dark:text-white"
+          >
+            Story of the day
+          </CardItem>
+          <CardItem
+            translateZ={20}
+            as="button"
+            className="text-neutral-500 max-w-sm mt-2 dark:text-neutral-300 text-xl font-bold"
+          >
+           Daniel in the Lion Den
+          </CardItem>
+          <CardItem
+            translateZ={20}
+            as="button"
+            className="text-neutral-500  max-w-sm mt-2 dark:text-neutral-300 text-left max-h-[72px] line-clamp-3"
+          >
+           Daniel, a faithful servant of God, was thrown into a den of lions but was protected by God's angels
+          </CardItem>
+          <CardItem
+            translateZ={20}
+            as="button"
+            className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold mt-4"
+          >
+           Read Now
+          </CardItem>
+        </div>
+      </CardBody>
+    </CardContainer>
+    <SOTD
+      title="Daniel in the Lion Den"
+      src="https://amcwebsitestorage.blob.core.windows.net/biblicalstoriesimg/bible project images/lion den.jpeg"
+      desc="Daniel, a faithful servant of God, was thrown into a den of lions but was protected by God's angels"
+      url="https://www.bible.com/en-GB/bible/111/DAN.6.NIV"
+    />
     </div>
   );
 }
---
+++ components/ui/story-of-the-day.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
+import Image from "next/image";
+
+export function SOTD({ title, src, desc, url }: { title: string, src: string, desc: string, url: string }) {
+
+  return (
+    <CardContainer className="inter-var">
+      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-lvh  h-auto rounded-xl p-6 border flex gap-4">
+    
+        <CardItem translateZ="100" className="w-2/3 mt-4">
+          <Image
+            src={src}
+            height="1000"
+            width="1000"
+            className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+            alt="thumbnail"
+          />
+        </CardItem>
+        <div className="flex justify-between items-left  flex-col">
+          <CardItem
+            translateZ={20}
+            as="a"
+            href="https://twitter.com/mannupaaji"
+            target="__blank"
+            className="text-3xl font-bold text-neutral-600 dark:text-white"
+          >
+            Story of the day
+          </CardItem>
+          <CardItem
+            translateZ={20}
+            as="button"
+            className="text-neutral-500 max-w-sm mt-2 dark:text-neutral-300 text-xl font-bold"
+          >
+           {title}
+          </CardItem>
+          <CardItem
+            translateZ={20}
+            as="button"
+            className="text-neutral-500  max-w-sm mt-2 dark:text-neutral-300 text-left max-h-[72px] line-clamp-3"
+          >
+            {desc}
+          </CardItem>
+          <CardItem
+            translateZ={20}
+            as="button"
+            className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold mt-4 cursor-pointer"
+            onClick={() => {            
+              window.open(url, "_blank");
+            }}
+          >
+            Read Now
+          </CardItem>
+        </div>
+      </CardBody>
+    </CardContainer>
+  );
+}
